404a2eb82fc27ea2593bf4161649537b28b3e999
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-06-14 03:33:04 +0200

    config details comp with api

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,4 +12,8 @@ export class DataService {
     return this.http.get('https://jsonplaceholder.typicode.com/users')
   }
 
+  getUser(userId) {
+    return this.http.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+  }
+
 }
