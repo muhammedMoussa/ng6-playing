0a8d69e492ca6103ae16f7341c9f91d7ca7aa561
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-06-14 03:56:55 +0200

    added angular animations

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(-15px)' }),
+            stagger(
+              '30ms',
+              animate(
+                '500ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0px)' })
+              )
+            )
+          ],
+          { optional: true }
+        ),
+        query(':leave', animate('50ms', style({ opacity: 0 })), {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
 export class PostsComponent implements OnInit {
 
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,13 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(-15px)' }),
+            stagger(
+              '30ms',
+              animate(
+                '500ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0px)' })
+              )
+            )
+          ],
+          { optional: true }
+        ),
+        query(':leave', animate('50ms', style({ opacity: 0 })), {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
+
 export class UsersComponent implements OnInit {
 
   users: Object
